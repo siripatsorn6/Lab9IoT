b1ab71e1f670e89058e9b4e59d481b02e4af0cfe
Siripatsorn <113875544+siripatsorn6@users.noreply.github.com>
2022-10-03 14:53:43 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,140 @@
+#include <Arduino.h>
+#include "SHTC3.h"
+#include <Firebase_ESP_Client.h>
+#include <ESP8266WiFi.h>
+
+
+// Provide the token generation process info.
+#include "addons/TokenHelper.h"
+// Provide the RTDB payload printing info and other helper functions.
+#include "addons/RTDBHelper.h"
+
+// Insert your network credentials
+#define WIFI_SSID "iotlab8"                  //เปลี่ยน
+#define WIFI_PASSWORD "kimmy0609"          //เปลี่ยน
+
+// Insert Firebase project API Key
+#define API_KEY "AIzaSyAdJVXDQPIOO8vKSCmeyZUlGfpTRU13VSk"         //เปลี่ยน
+
+// Insert Authorized Email and Corresponding Password
+#define USER_EMAIL "siripatsorn06092544@gmail.com"            //เปลี่ยน
+#define USER_PASSWORD "kimmy6944"      //เปลี่ยน
+
+// Insert RTDB URLefine the RTDB URL
+#define DATABASE_URL "https://esp8266-shtc3-88f96-default-rtdb.asia-southeast1.firebasedatabase.app/"       //เปลี่ยน
+
+// Define Firebase objects
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+// Variable to save USER UID
+String uid;
+
+// Variables to save database paths
+String databasePath;
+String tempPath;
+String humPath;
+
+// SHTC3 sensor
+SHTC3 shtc3(Wire); // I2C
+float temperature;
+float humidity;
+
+// Timer variables (send new readings every three minutes)
+unsigned long sendDataPrevMillis = 0;
+unsigned long timerDelay = 180000;
+
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  Serial.println();
+}
+
+// Write float values to the database
+void sendFloat(String path, float value){
+  if (Firebase.RTDB.setFloat(&fbdo, path.c_str(), value)){
+    Serial.print("Writing value: ");
+    Serial.print (value);
+    Serial.print(" on the following path: ");
+    Serial.println(path);
+    Serial.println("PASSED");
+    Serial.println("PATH: " + fbdo.dataPath());
+    Serial.println("TYPE: " + fbdo.dataType());
+  }
+  else {
+    Serial.println("FAILED");
+    Serial.println("REASON: " + fbdo.errorReason());
+  }
+}
+
+void setup(){
+  Serial.begin(115200);
+  Wire.begin();
+  initWiFi();
+
+  // Assign the api key (required)
+  config.api_key = API_KEY;
+
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
+  // Assign the RTDB URL (required)
+  config.database_url = DATABASE_URL;
+
+  Firebase.reconnectWiFi(true);
+  fbdo.setResponseSize(4096);
+
+  // Assign the callback function for the long running token generation task */
+  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+
+  // Assign the maximum retry of token generation
+  config.max_token_generation_retry = 5;
+
+  // Initialize the library with the Firebase authen and config
+  Firebase.begin(&config, &auth);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
+
+  // Update database path
+  databasePath = "/UsersData/" + uid;
+
+  // Update database path for sensor readings
+  tempPath = databasePath + "/temperature"; // --> UsersData/<user_uid>/temperature
+  humPath = databasePath + "/humidity"; // --> UsersData/<user_uid>/humidity
+}
+
+void loop(){
+  shtc3.begin(true);
+  shtc3.sample();
+
+  // Send new readings to database
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > timerDelay || sendDataPrevMillis == 0)){
+    sendDataPrevMillis = millis();
+
+    // Get latest sensor readings
+    temperature = shtc3.readTempC();
+    humidity = shtc3.readHumidity();
+
+    // Send readings to database:
+    sendFloat(tempPath, temperature);
+    sendFloat(humPath, humidity);
+  }
+}
